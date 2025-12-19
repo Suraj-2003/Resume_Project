e28aa74ad9768dc8e4696c92712e71c461837143
Suraj More <moresuraj9511@gmail.com>
2025-12-19 10:41:40 +0530

    Added component for highlights

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Personal } from './components/personal/personal';
+import { Education } from './components/education/education';
+import { Skills } from './components/skills/skills';
+import { Tools } from './components/tools/tools';
+import { Achievements } from './components/achievements/achievements';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Personal,Education,Skills,Tools,Achievements],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
